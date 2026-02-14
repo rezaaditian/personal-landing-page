385164b0559633af40985232cf941ef8ffda441b
Reza A <rezaaditian24@gmail.com>
2026-02-14 11:49:28 +0700

    templating and separate per component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,157 @@
+
+
+const Footer = () => {
+    return (
+        <section className="footer">
+            <div className="footer-wrapper">
+                <div className="footer-top">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12 col-sm-6 col-lg-4 mt-5 mt-lg-0">
+                                <div className="footer-col-one">
+                                    <img src="img/LogoWhite.png" className="footer-logo" alt="footer logo"/>
+                                    <p className="p text-white line-height-7 mb-30 footer-col-one-texts">
+                                        At vero eos et accusamus et iusto odio dign ducimus qui blanditiis praesentium
+                                        volup deleniti atque corrupti quos dolores et molestias excepturi sint occaecati
+                                    </p>
+                                    <h3 className="h3 fw-500 line-height-3 text-white fw-500">
+                                        credesign@gmail.com
+                                    </h3>
+                                </div>
+                            </div>
+                            <div className="col-6 col-sm-6 col-lg-2 mt-5 mt-lg-0">
+                                <h4 className="h4 fw-500 text-white">
+                                    Explore Link
+                                </h4>
+                                <div className="footer-col-links">
+                                    <ul className="footer-col-link-container">
+                                        <li className="footer-link-item">
+                                            <a className="fw-400 p line-height-7 text-white" href="about.html">About</a>
+                                        </li>
+                                        <li className="footer-link-item">
+                                            <a className="fw-400 p line-height-7 text-white" href="resume.html">Resume</a>
+                                        </li>
+                                        <li className="footer-link-item">
+                                            <a className="fw-400 p line-height-7 text-white"
+                                                href="portfolio.html">Portfolio</a>
+                                        </li>
+                                        <li className="footer-link-item">
+                                            <a className="fw-400 p line-height-7 text-white" href="blog.html">Blog</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-6 col-sm-6 col-lg-2 mt-5 mt-lg-0">
+                                <h4 className="h4 fw-500 text-white">
+                                    My Services
+                                </h4>
+                                <div className="footer-col-links">
+                                    <ul className="footer-col-link-container">
+                                        <li className="footer-link-item">
+                                            <a className="fw-400 p line-height-7 text-white" href="services.html">UI/UX
+                                                Design</a>
+                                        </li>
+                                        <li className="footer-link-item">
+                                            <a className="fw-400 p line-height-7 text-white" href="services.html">Mobile
+                                                App</a>
+                                        </li>
+                                        <li className="footer-link-item">
+                                            <a className="fw-400 p line-height-7 text-white" href="services.html">Graphics
+                                                Design</a>
+                                        </li>
+                                        <li className="footer-link-item">
+                                            <a className="fw-400 p line-height-7 text-white" href="services.html">Web
+                                                Developer</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-12 col-sm-6 col-lg-4 mt-5 mt-lg-0">
+                                <h4 className="h4 fw-500 text-white">
+                                    Follow me
+                                </h4>
+                                <div className="footer-social-media">
+                                    <ul className="footer-social-container d-flex align-items-center mb-35">
+                                        <li className="footer-social-item-2">
+                                            <a href="https://www.facebook.com/">
+                                                <div className="orange-bg-icon">
+                                                    <i className="fa-brands fa-facebook-f text-white h4"></i>
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li className="footer-social-item-2">
+                                            <a href="https://twitter.com/">
+                                                <div className="orange-bg-icon">
+                                                    <i className="fa-brands fa-twitter text-white h4"></i>
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li className="footer-social-item-2">
+                                            <a href="https://dribbble.com/">
+                                                <div className="orange-bg-icon">
+                                                    <i className="fa-brands fa-dribbble text-white h4"></i>
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li className="footer-social-item-2">
+                                            <a href="https://www.behance.net/">
+                                                <div className="orange-bg-icon">
+                                                    <i className="fa-brands fa-behance text-white h4"></i>
+                                                </div>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="footer-contact">
+                                    <div className="footer-contact-item d-flex align-items-center mb-20">
+                                        <i className="fa-solid fa-location-dot footer-contact-icon mr-10 orange-color"></i>
+                                        <p className="p text-white line-height-7 fw-400">
+                                            202 Dog Hill Lane Beloit,KS 67420
+                                        </p>
+                                    </div>
+                                    <div className="footer-contact-item d-flex align-items-center mb-20">
+                                        <i className="fa-solid fa-phone-volume footer-contact-icon mr-10 orange-color"></i>
+                                        <p>
+                                            <a className="p text-white line-height-7 fw-400" href="tel:1-800-915-6270">
+                                                1-800-915-6270
+                                            </a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-bottom">
+                    <div className="container">
+                        <div className="footer-bottom-container">
+                            <div className="row">
+                                <div className="footerbottom-left col-12 col-sm-5 col-md-6 text-start">
+                                    <p className="p text-white line-height-7">
+                                        All rights reserved &copy; 2023 Credesign
+                                    </p>
+                                </div>
+                                <div className="footerbottom-right col-12 col-sm-7 col-md-6">
+                                    <ul className="d-flex justify-content-sm-end">
+                                        <li className="mr-100">
+                                            <a className="text-white p" href="termsandconditions.html">
+                                                Terms & Condition
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a className="text-white p" href="privacypolicy.html">
+                                                Privacy policy
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Footer
---
+++ app/components/Portfolio.tsx
@@ -0,0 +1,165 @@
+
+
+const Portfolio = () => {
+    return (
+        <section className="portfolio">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="portfolio-wrapper">
+                            <div className="section-heading-middle">
+                                <div className="sub-heading d-flex align-items-center mx-auto">
+                                    <img src="img/orangeDot.png" alt="orange-dot"/>
+                                    <p>My Portfolio</p>
+                                </div>
+                                <h2 className="black-color line-height-3 h2 text-uppercase text-center">
+                                    VISIT MY PORTFOLIO
+                                </h2>
+                            </div>
+                            <div className="mt-50 row-mobile-margin">
+                                <div className="controls d-flex justify-content-center flex-wrap gap-1 gap-lg-4 mb-45">
+                                    <button type="button" className="control filter btn .outline-0 border-0 secondary-black"
+                                        data-filter=".all">All</button>
+                                    <button type="button" className="control filter btn .outline-0 border-0 secondary-black"
+                                        data-filter=".ui">UI/Ux Design</button>
+                                    <button type="button" className="control filter btn .outline-0 border-0 secondary-black"
+                                        data-filter=".branding">Branding Design</button>
+                                    <button type="button" className="control filter btn .outline-0 border-0 secondary-black"
+                                        data-filter=".graphic">Graphic Design</button>
+                                    <button type="button" className="control filter btn .outline-0 border-0 secondary-black"
+                                        data-filter=".app">App Development</button>
+                                    <button type="button" className="control filter btn .outline-0 border-0 secondary-black"
+                                        data-filter=".web">Web Design</button>
+                                </div>
+                                <div className="portfolio-massonary-container">
+                                    <div className="portfolio-massonary-items mix ui all position-relative">
+                                        <img className="img-fluid" src="img/portfolioOne.png" alt="portfolio-img"/>
+                                        <div className="portfolio-card-overlay w-100 position-absolute top-0">
+                                        </div>
+                                        <div
+                                            className="d-flex w-100 position-absolute bottom-0 left-0 justify-content-center align-items-center">
+                                            <div className="position-absolute mx-auto portfolio-card-overlay-contents">
+                                                <p className="text-white fw-400 line-height-7">Graphic Design</p>
+                                                <a href="portfolioDetails.html"
+                                                    className="h4 text-white fw-600 line-height-3">
+                                                    The best way to impress your web
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="portfolio-massonary-items mix graphic all position-relative">
+                                        <img className="img-fluid" src="img/portfolioTwo.png" alt="portfolio-img"/>
+                                        <div className="portfolio-card-overlay w-100 position-absolute top-0">
+                                        </div>
+                                        <div
+                                            className="d-flex w-100 position-absolute bottom-0 left-0 justify-content-center align-items-center">
+                                            <div className="position-absolute mx-auto portfolio-card-overlay-contents">
+                                                <p className="text-white fw-400 line-height-7">Graphic Design</p>
+                                                <a href="portfolioDetails.html"
+                                                    className="h4 text-white fw-600 line-height-3">
+                                                    The best way to impress your web
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="portfolio-massonary-items mix ui graphic all position-relative">
+                                        <img className="img-fluid" src="img/portfolioThree.png" alt="portfolio-img"/>
+                                        <div className="portfolio-card-overlay w-100 position-absolute top-0">
+                                        </div>
+                                        <div
+                                            className="d-flex w-100 position-absolute bottom-0 left-0 justify-content-center align-items-center">
+                                            <div className="position-absolute mx-auto portfolio-card-overlay-contents">
+                                                <p className="text-white fw-400 line-height-7">Graphic Design</p>
+                                                <a href="portfolioDetails.html"
+                                                    className="h4 text-white fw-600 line-height-3">
+                                                    The best way to impress your web
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="portfolio-massonary-items mix web all position-relative">
+                                        <img className="img-fluid" src="img/portfolioFour.png" alt="portfolio-img"/>
+                                        <div className="portfolio-card-overlay w-100 position-absolute top-0">
+                                        </div>
+                                        <div
+                                            className="d-flex w-100 position-absolute bottom-0 left-0 justify-content-center align-items-center">
+                                            <div className="position-absolute mx-auto portfolio-card-overlay-contents">
+                                                <p className="text-white fw-400 line-height-7">Graphic Design</p>
+                                                <a href="portfolioDetails.html"
+                                                    className="h4 text-white fw-600 line-height-3">
+                                                    The best way to impress your web
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="portfolio-massonary-items mix app all position-relative">
+                                        <img className="img-fluid" src="img/portfolioFive.png" alt="portfolio-img"/>
+                                        <div className="portfolio-card-overlay w-100 position-absolute top-0">
+                                        </div>
+                                        <div
+                                            className="d-flex w-100 position-absolute bottom-0 left-0 justify-content-center align-items-center">
+                                            <div className="position-absolute mx-auto portfolio-card-overlay-contents">
+                                                <p className="text-white fw-400 line-height-7">Graphic Design</p>
+                                                <a href="portfolioDetails.html"
+                                                    className="h4 text-white fw-600 line-height-3">
+                                                    The best way to impress your web
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="portfolio-massonary-items mix branding all position-relative">
+                                        <img className="img-fluid" src="img/portfolioSix.png" alt="portfolio-img"/>
+                                        <div className="portfolio-card-overlay w-100 position-absolute top-0">
+                                        </div>
+                                        <div
+                                            className="d-flex w-100 position-absolute bottom-0 left-0 justify-content-center align-items-center">
+                                            <div className="position-absolute mx-auto portfolio-card-overlay-contents">
+                                                <p className="text-white fw-400 line-height-7">Graphic Design</p>
+                                                <a href="portfolioDetails.html"
+                                                    className="h4 text-white fw-600 line-height-3">
+                                                    The best way to impress your web
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="portfolio-massonary-items mix web all position-relative">
+                                        <img className="img-fluid" src="img/portfolioSeven.png" alt="portfolio-img"/>
+                                        <div className="portfolio-card-overlay w-100 position-absolute top-0">
+                                        </div>
+                                        <div
+                                            className="d-flex w-100 position-absolute bottom-0 left-0 justify-content-center align-items-center">
+                                            <div className="position-absolute mx-auto portfolio-card-overlay-contents">
+                                                <p className="text-white fw-400 line-height-7">Graphic Design</p>
+                                                <a href="portfolioDetails.html"
+                                                    className="h4 text-white fw-600 line-height-3">
+                                                    The best way to impress your web
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="portfolio-massonary-items mix app all position-relative">
+                                        <img className="img-fluid" src="img/portfolioEight.png" alt="portfolio-img"/>
+                                        <div className="portfolio-card-overlay w-100 position-absolute top-0">
+                                        </div>
+                                        <div
+                                            className="d-flex w-100 position-absolute bottom-0 left-0 justify-content-center align-items-center">
+                                            <div className="position-absolute mx-auto portfolio-card-overlay-contents">
+                                                <p className="text-white fw-400 line-height-7">Graphic Design</p>
+                                                <a href="portfolioDetails.html"
+                                                    className="h4 text-white fw-600 line-height-3">
+                                                    The best way to impress your web
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Portfolio
